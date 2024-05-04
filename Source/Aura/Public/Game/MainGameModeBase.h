250897b037b5494fa55d1e87f2931809a1fc8d5e
Sam Hill <SJHill95@github.com>
2024-05-04 15:07:52 +0100

    Setup game mode base

---
+++ Source/Aura/Public/Game/MainGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Sam Hill
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MainGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AMainGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
